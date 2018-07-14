b80ab5272292affb989463493586b408dd913684
Marius Ciotlos <ciotlos@gmail.com>
2018-07-14 22:37:33 +0300

    Updated layout a bit more

--- alarm_control_panel-card/alarm_control_panel-card.js
+++ alarm_control_panel-card/alarm_control_panel-card.js
@@ -18,14 +18,29 @@ class AlarmControlPanelCard extends HTMLElement {
     const content = document.createElement('div');
     const style = document.createElement('style');
     style.textContent = `
+      ha-card {
+        padding-bottom: 16px;
+        position: relative;
+      }
+      ha-icon {
+        color: var(--paper-item-icon-color);
+        position: absolute;
+        right: 20px;
+        top: 32px;
+      }
       paper-input {
         margin: auto;
         max-width: 200px;
       }
+      .state {
+        margin-left: 20px;
+        position: relative;
+        bottom: 16px;
+        color: var(--secondary-text-color)
+      }
       .pad {
         display: flex;
         justify-content: center;
-        margin-bottom: 24px;
       }
       .pad div {
         display: flex;
@@ -33,10 +48,15 @@ class AlarmControlPanelCard extends HTMLElement {
       }
       .pad paper-button {
         width: 80px;
+        margin-bottom: 8px;
+      }
+      .actions {
+        margin: 0 8px;
+        display: flex;
+        justify-content: center;
       }
       .actions paper-button {
-        min-width: 160px;
-        margin-bottom: 16px;
+        min-width: 150px;
         color: var(--primary-color);
       }
       paper-button.disarm {
@@ -64,17 +84,65 @@ class AlarmControlPanelCard extends HTMLElement {
     return !code || !format || code.length > 0;
   }
 
+  _getIcon(entity) {
+    let state = entity.state;
+    switch (state) {
+      case 'disarmed':
+        return 'hass:bell-outline'
+      case 'armed_home':
+        return 'hass:bell-plus'
+      case 'pending':
+      case 'armed_away':
+        return 'hass:bell'
+    }
+    return 'hass:bell'
+  }
+
+  _translateState(state) {
+    switch (state) {
+      case 'disarmed':
+        return 'Disarmed'
+      case 'armed_home':
+        return 'Armed home'
+      case 'pending':
+        return 'Pending'
+      case 'armed_away':
+        return 'Armed away'
+    } 
+    return 'Unknown'
+  }
+
   _updateCardContent(entity) {
     const root = this.shadowRoot;
     const _armVisible = entity.state === 'disarmed';
     const _disarmVisible = (this._armedStates.includes(entity.state) || entity.state === 'pending' || entity.state === 'triggered');
     const codeDisabled = !(_disarmVisible || _armVisible)?'disabled':'';
-    const buttonsDisabled = !this._validateCode(this._enteredCode, entity.attributes.code_format)?'disabled':'';
+    const buttonsDisabled = (!this._validateCode(this._enteredCode, entity.attributes.code_format))?'':'disabled';
+    if (!this._config.title) {
+      this.shadowRoot.lastChild.header = this._translateState(entity.state);
+    }
     const content = `
+      <ha-icon icon='${this._getIcon(entity)}'></ha-icon>
+      ${this._config.title?`<div class='state'>${this._translateState(entity.state)}</div>`:''}
+      <div class="actions">
+        ${_disarmVisible ? `
+          <paper-button raised class="disarm" ${buttonsDisabled} id='disarm'>
+            Disarm
+          </paper-button>
+        `: ''}
+        ${_armVisible ? `
+          <paper-button raised ${buttonsDisabled} id='arm_home'>
+            Arm Home
+          </paper-button>
+          <paper-button raised ${buttonsDisabled} id='arm_away'>
+            Arm Away
+          </paper-button>
+        `: ''}
+      </div>
       ${entity.attributes.code_format ? `
         <paper-input
           label="Alarm code"
-          value="${this._enteredCode}"
+          value=""
           type="password"
           ${codeDisabled}
         ></paper-input>
@@ -96,27 +164,12 @@ class AlarmControlPanelCard extends HTMLElement {
               <paper-button ${codeDisabled} data-digit="3" raised>3</paper-button>
               <paper-button ${codeDisabled} data-digit="6" raised>6</paper-button>
               <paper-button ${codeDisabled} data-digit="9" raised>9</paper-button>
-              <paper-button ${codeDisabled} raised>
+              <paper-button ${codeDisabled} id='clear' raised>
                 Clear
               </paper-button>
             </div>
           </div>
       `: ''}
-      <div class="layout horizontal center-justified actions">
-        ${_disarmVisible ? `
-          <paper-button raised class="disarm" on-click="_callService" data-service="alarm_disarm" ${buttonsDisabled}">
-            Disarm
-          </paper-button>
-        `: ''}
-        ${_armVisible ? `
-          <paper-button raised on-click="_callService" data-service="alarm_arm_home" ${buttonsDisabled}>
-            Arm Home
-          </paper-button>
-          <paper-button raised on-click="_callService" data-service="alarm_arm_away" ${buttonsDisabled}">
-            Arm Away
-          </paper-button>
-        `: ''}
-      </div>
     `;
 
 
