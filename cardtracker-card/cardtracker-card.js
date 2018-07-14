ffdf59ddda5e8c7b1e0b10cd47ff1f367f1f95e2
Marius Ciotlos <ciotlos@gmail.com>
2018-07-14 16:52:30 +0300

    Updated card-tracker

--- cardtracker-card/cardtracker-card.js
+++ cardtracker-card/cardtracker-card.js
@@ -42,32 +42,37 @@ class CardTracker extends HTMLElement {
           paper-button {
             float: right;
           }
+          td.has_update {
+            color: red;
+            font-weight: bold;
+          }
         `;
     content.innerHTML = `
-      <table>
+      <table id='content'>
         <thead><tr><th>Name</th><th>Current</th><th>Available</th></tr></thead>
         <tbody id='container'>
         </tbody>
       </table>
       <div class='button'>
-        <paper-button raised>Update All</paper-button>
+        <paper-button raised id='update'>Update All</paper-button>
+        <paper-button raised id='check'>Check</paper-button>
       </div>
     `;
     card.header = cardConfig.title
     card.appendChild(content);
     card.appendChild(style);
-    card.querySelector('paper-button').addEventListener('click', event => {
-      const list = this._filterCards(this.myhass.states[config.entity].attributes);
-      list.forEach(el => {
-          this.myhass.callService('custom_cards', 'update_cards', {'card': el[0] });
-      });
+    card.querySelector('#update').addEventListener('click', event => {
+      this.myhass.callService('custom_cards', 'update_cards', {});
+    });
+    card.querySelector('#check').addEventListener('click', event => {
+      this.myhass.callService('custom_cards', 'check_versions', {});
     });
     root.appendChild(card);
     this._config = cardConfig;
   }
 
   _filterCards(attributes) {
-    return Object.entries(attributes).filter(elem => elem[1].update !== 'False' && elem[0] != "friendly_name");
+    return Object.entries(attributes).filter(elem => elem[0] != "friendly_name");
   }
 
   set hass(hass) {
@@ -75,22 +80,19 @@ class CardTracker extends HTMLElement {
     const root = this.shadowRoot;
     this.myhass = hass;
 
+    console.log((hass.states[config.entity]))
     if (hass.states[config.entity]) {
       const list = this._filterCards(hass.states[config.entity].attributes);
       this.style.display = 'block';
       if (list !== undefined && list.length > 0) {
         const updated_content = `
           ${list.map(elem => `
-            <tr><td>${elem[0]}</td><td>${elem[1].installed!='False'?elem[1].installed:'n/a'}</td><td>${elem[1].version!='False'?elem[1].version:'n/a'}</td></tr>
+            <tr><td>${elem[0]}</td><td>${elem[1].local?elem[1].local:'n/a'}</td><td class='${elem[1].has_update?'has_update':''}'>${elem[1].remote?elem[1].remote:'n/a'}</td></tr>
           `).join('')}
         `;
         root.getElementById('container').innerHTML = updated_content;
-      } else {
-        this.style.display = 'none';
       }
       root.lastChild.hass = hass;
-    } else {
-      this.style.display = 'none';
     }
   }
   getCardSize() {
