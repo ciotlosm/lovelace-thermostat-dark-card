384ddf69150f000884f049f323728415de13f71e
Marius Ciotlos <ciotlos@gmail.com>
2018-08-07 12:41:19 +0300

    Renamed a few things

--- thermostat-card/webpack.config.js
+++ thermostat-card/webpack.config.js
@@ -1,19 +1,18 @@
-const card = 'thermostat-card';
 module.exports = {
-    entry: `./src/${card}.js`,
-    output: {
-        path: __dirname,
-        filename: `${card}.bundle.js`
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: [
-                    { loader: "to-string-loader" },
-                    { loader: "css-loader" }
-                ]
-            }
+  entry: './src/card.js',
+  output: {
+    path: __dirname,
+    filename: 'thermostat-card.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          { loader: "to-string-loader" },
+          { loader: "css-loader" }
         ]
-    },
+      }
+    ]
+  },
 };
