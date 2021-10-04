a123d3bf31cd2c7ce22cedfe814a3b24ded128ad
Miklos Szanyi <miklos.szanyi@gmail.com>
2021-10-04 11:48:53 +0300

    Add on off toggle and more info (#270)
    * Update container config to meet new HA config
    
    * Enable source maps in dev build
    
    * Add off state toggle and more info on touch hold and right click
    
    * Fix build errors
    
    * Fix power button clikable area
    
    * Adjust power toggle colorw hile heating/cooling
    
    Co-authored-by: Miklos Szanyi <miklos.szanyi@gmil.com>

--- src/const.ts
+++ src/const.ts
@@ -1,6 +1,7 @@
 export const HVAC_HEATING = 'heating';
 export const HVAC_COOLING = 'cooling';
 export const HVAC_IDLE = 'idle';
+export const HVAC_OFF = 'off';
 
 export const PRESET_ECO = 'eco';
 export const PRESET_AWAY = 'away';
