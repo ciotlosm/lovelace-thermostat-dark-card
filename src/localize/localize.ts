4ae366a05a2c3197490a14bd113bb2b3883d97bf
misa1515 <61636045+misa1515@users.noreply.github.com>
2023-11-07 08:49:43 +0100

    Update localize.ts (#306)

--- src/localize/localize.ts
+++ src/localize/localize.ts
@@ -1,10 +1,12 @@
 import * as en from './languages/en.json';
 import * as nb from './languages/nb.json';
+import * as sk from './languages/sk.json';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const languages: any = {
   en: en,
   nb: nb,
+  sk: sk,
 };
 
 export function localize(string: string, search = '', replace = ''): string {
